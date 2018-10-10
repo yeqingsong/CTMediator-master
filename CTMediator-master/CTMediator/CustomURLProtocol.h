39428cfb0479cf5d1ffb570aed9534329028fc7a
shoule <253281479@qq.com>
2018-10-10 16:17:05 +0800

    no message

---
+++ CTMediator-master/CTMediator/CustomURLProtocol.h
@@ -0,0 +1,13 @@
+//
+//  CustomURLProtocol.h
+//  CTMediator
+//
+//  Created by shoule on 2018/9/28.
+//  Copyright © 2018年 casa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomURLProtocol : NSURLProtocol
+
+@end
---
+++ CTMediator-master/CTMediator/CustomURLProtocol.m
@@ -0,0 +1,13 @@
+//
+//  CustomURLProtocol.m
+//  CTMediator
+//
+//  Created by shoule on 2018/9/28.
+//  Copyright © 2018年 casa. All rights reserved.
+//
+
+#import "CustomURLProtocol.h"
+
+@implementation CustomURLProtocol
+
+@end
